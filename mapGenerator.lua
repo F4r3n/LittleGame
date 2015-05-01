84b6fac96fbe3e0cfd6289f6207a0637dfd6ef3b
F4r3n <guillaume_k@hotmail.fr>
2015-05-01 14:47:48 +0200

    init map

--- mapGenerator.lua
+++ mapGenerator.lua
@@ -3,7 +3,8 @@ local MapGenerator = {
 	height = 100,
 	numberCenters = 5,
 	radius = 10,
-	centers = {}
+	centers = {},
+	tab = {}
 }
 
 MapGenerator.__index = MapGenerator
@@ -23,6 +24,20 @@ function MapGenerator:createCenters()
 	for i=1,self.numberCenters do
 		table.insert(self.centers,Point.new(math.random()%self.width,math.random()%self.height,1,self.radius))
 	end
+end
+
+function MapGenerator:initMap()
+	for i=1,self.height do
+		self.tab[i] = {}
+		for j=1,self.width do
+			self.tab[i][j] = 0
+			for _,v in ipairs(self.centers) do
+	--			if math.sqrt((v.x-j)*(v.x-j) + (v.y-i)*(v.y-i)) <
+			end
+		end
+	end
+
+
 end
 
 return MapGenerator
